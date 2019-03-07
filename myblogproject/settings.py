af8923b00731ae85533d6a96b962a5d35be2d06a
knight7024 <knight7024@naver.com>
2019-03-07 12:00:37 +0900

    heroku commit

--- myblogproject/settings.py
+++ myblogproject/settings.py
@@ -139,4 +139,4 @@ db_from_env = dj_database_url.config(conn_max_age=500)
 DATABASES['default'].update(db_from_env)
 
 
-STATICFILES_STORAGE='whitenoise.django.GzipManifestStaticFilesStorage'
+STATICFILES_STORAGE='whitenoise.storage.CompressedManifestStaticFilesStorage'
