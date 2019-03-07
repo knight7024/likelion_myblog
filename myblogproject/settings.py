ece00dbfb06275e9c7cd9f554d89d24590b3ac3c
knight7024 <knight7024@naver.com>
2019-03-07 12:13:40 +0900

    heroku commit

--- myblogproject/settings.py
+++ myblogproject/settings.py
@@ -123,14 +123,13 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 STATICFILES_DIRS = [
-    # os.path.join(BASE_DIR, 'portfolioapp', 'static'),
-    # os.path.join(BASE_DIR, 'myblogproject', 'static')
-    os.path.join(BASE_DIR, 'static'),
+    os.path.join(BASE_DIR, 'portfolioapp', 'static'),
+    os.path.join(BASE_DIR, 'myblogproject', 'static')
 ]
 
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 
-MEDIA_ROOT = os.path.join(BASE_DIR, 'mediafiles')
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 MEDIA_URL = '/media/'
 
 
