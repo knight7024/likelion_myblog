877c97b0e1fca87ac80daed4d5a1941b63fc2ca8
knight7024 <knight7024@naver.com>
2019-03-07 12:19:28 +0900

    heroku commit

--- myblogproject/settings.py
+++ myblogproject/settings.py
@@ -124,9 +124,6 @@ USE_TZ = True
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATIC_URL = "/static/"
 
-django_heroku.settings(locals())
-
-
 # Heroku: Update database configuration from $DATABASE_URL.
 import dj_database_url
 db_from_env = dj_database_url.config(conn_max_age=500)
